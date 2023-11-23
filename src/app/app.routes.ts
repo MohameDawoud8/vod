818402e1f4219b6af8a440ad3a3d59659161c67a
root <root@ERCAITLAP-0000>
2023-11-23 21:54:40 +0200

    add posts resource routs with 404 fallback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PostComponent } from './post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostsComponent } from './posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/blogs', pathMatch: 'full' },
+
+  // RESTful Resources
+  { path: 'posts', title: 'Blogs', component: PostsComponent },
+  { path: 'posts/:id', title: 'Post', component: PostComponent },
+
+  // Fallback 404
+  { path: '**', component: PageNotFoundComponent },
+];
