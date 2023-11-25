8211c7d0e063e205d6beb4238c54e13758acec65
root <root@ERCAITLAP-0000>
2023-11-25 19:51:29 +0200

    add pic

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { UserService } from './services/user.service';
+import { PostService } from './services/post.service';
+import { CommentService } from './services/comment.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    UserService,
+    PostService,
+    CommentService,
+    { provide: 'API_URL', useValue: 'https://jsonplaceholder.typicode.com' },
+  ],
 };
