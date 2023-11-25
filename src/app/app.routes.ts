0d83abc316e69ca7ae7547c335738a7d492cf8b9
root <root@ERCAITLAP-0000>
2023-11-25 16:16:43 +0200

    update routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { PostsComponent } from './posts/posts.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/blogs', pathMatch: 'full' },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
 
   // RESTful Resources
-  { path: 'posts', title: 'Blogs', component: PostsComponent },
+  { path: 'posts', title: 'Posts', component: PostsComponent },
   { path: 'posts/:id', title: 'Post', component: PostComponent },
 
   // Fallback 404
